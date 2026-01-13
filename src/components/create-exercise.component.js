003188a3184ecc7cb3827f883847edea56441304
yusufriswan <119690890+yusufriswan@users.noreply.github.com>
2026-01-13 09:49:44 +0530

    Fix URL in axios GET request

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -23,7 +23,7 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://watch-dev.vercel.app/')
+    axios.get('https://watch-dev.vercel.app')
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
