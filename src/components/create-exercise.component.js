90cc6e76ec8acca6b882741f694b5ea9420cf933
yusufriswan <119690890+yusufriswan@users.noreply.github.com>
2026-01-13 10:00:52 +0530

    Fix URL formatting in axios post request

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -74,7 +74,7 @@ export default class CreateExercise extends Component {
 
     console.log(exercise);
 
-    axios.post('https://watch-dev.vercel.app'+'/exercises/add', exercise)
+    axios.post('https://watch-dev.vercel.app/exercises/add', exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
